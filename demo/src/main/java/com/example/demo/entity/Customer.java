af8834ea53c6ee42d766225371df44c6295e8725
madongsen <madongsen0926@qq.com>
2021-12-06 16:58:14 +0800

    加构造函数

--- demo/src/main/java/com/example/demo/entity/Customer.java
+++ demo/src/main/java/com/example/demo/entity/Customer.java
@@ -5,4 +5,9 @@ import org.springframework.core.serializer.Serializer;
 public class Customer {
     private int id;
    private String name;
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
