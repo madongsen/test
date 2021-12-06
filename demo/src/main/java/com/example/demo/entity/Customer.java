40ff2756861ffce6401b4870e647d7e8faffcc99
madongsen <madongsen0926@qq.com>
2021-12-06 17:00:52 +0800

    加get set

--- demo/src/main/java/com/example/demo/entity/Customer.java
+++ demo/src/main/java/com/example/demo/entity/Customer.java
@@ -10,4 +10,20 @@ public class Customer {
         this.id = id;
         this.name = name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
